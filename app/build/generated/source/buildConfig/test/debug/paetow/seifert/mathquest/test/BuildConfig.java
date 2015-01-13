46bae1edd1c2431471c15c7d1b67431e2d0ebda8
xoif <zippoxtc@gmail.com>
2015-01-13 16:01:01 +0100

    für neue Version angepasst

--- app/build/generated/source/buildConfig/test/debug/paetow/seifert/mathquest/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/test/debug/paetow/seifert/mathquest/test/BuildConfig.java
@@ -5,7 +5,7 @@ package paetow.seifert.mathquest.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "paetow.seifert.mathquest.test";
+  public static final String APPLICATION_ID = "paetow.seifert.mathquest.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = -1;
